7f0c9d49b7a65dcaa48df1ab7d5928ccc0f923ef
Liyew <118590747+woletee@users.noreply.github.com>
2025-01-21 12:16:06 +0900

    Update logic.py

--- amazing_python_scripts/2048_game/logic.py
+++ amazing_python_scripts/2048_game/logic.py
@@ -5,3 +5,11 @@ def get_start_game():
         mat.append([0,0,0,0,0])
         add_two(mat)
         return mat
+  def add_two(mat):
+    r=random.randint(0,4)
+    c=random.randint(0,4)
+    while(mat[r]!=0):
+        r=random.randint(0,4)
+        c=random.randint(0,4)
+    mat[r]=2
+    
