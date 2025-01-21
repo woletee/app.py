4bbd7c538a088f5c6a30e10445e1357a11f135ab
Liyew <118590747+woletee@users.noreply.github.com>
2025-01-21 12:31:08 +0900

    Update logic.py

--- amazing_python_scripts/2048_game/logic.py
+++ amazing_python_scripts/2048_game/logic.py
@@ -46,3 +46,12 @@ def compress(mat):
                     changed=True
                 pos+=1
     return new_mat, changed
+def merge(mat):
+    changed=False
+    for i in range(4):
+        for j in range(3):
+            if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
+                mat[i][j]*=2
+                mat[i][j+1]=0
+                changed=True
+    return mat,changed
